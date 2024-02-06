895130cf137c979c5515985252f419b1218735d3
Afpa <alex.nesvit2000@gmail.com>
2024-02-06 13:22:04 +0100

    app.use(express.json())

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import express from 'express'
 const PORT = 5000;
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    console.log(req.query);
+app.post('/', (req, res) => {
+    console.log(req.body);
     res.status(200).json("Server is working")
 })
 
